cc509afcb9d1bf0f9a65693f2d37791d3db4dd1b
Borislav Angelov <borislav.angelov@gmail.com>
2013-04-23 03:13:30 +0300

    Adding bamboo pull requests app
    Signed-off-by: Borislav Angelov <borislav.angelov@gmail.com>

---
+++ bamboo.rb
@@ -0,0 +1,90 @@
+require 'sinatra'
+require 'sinatra/config_file'
+require 'bamboo-client'
+require 'octokit'
+require 'json'
+
+config_file 'config.yml'
+
+before do
+	@github_client = Octokit::Client.new(:login => settings.github['username'], :oauth_token => settings.github['token'])
+end
+
+post '/build-plan/:key' do |key|
+  begin
+    bamboo_client = Bamboo::Client.for(:rest, settings.bamboo['url'])
+    bamboo_client.login(settings.bamboo['username'], settings.bamboo['password'])
+
+    plan = bamboo_client.plan_for(key)
+    if plan.enabled?
+
+  		data = JSON.parse(request.body.read)
+  		puts data
+
+    	# Update Status
+    	# Run build-plan
+    	# Update status for completed
+      "Vsichko e po plan"
+    else
+      "This plan is not enabled in Bamboo database"
+    end
+
+  rescue RestClient::ResourceNotFound
+  	"This plan does not exist in Bamboo database"
+  rescue SocketError
+  	"Wrong Bamboo URL Address! Please check your configuration file."
+  end
+end
+
+get '/manage-hooks' do
+  @repo_name = params[:repo_name]
+
+  if @repo_name
+    begin
+  	  @hooks = @github_client.hooks(@repo_name)
+
+  	  puts @hooks
+    rescue Octokit::NotFound
+  	  @message = "This repository is not found in GitHub (example: borislav-angelov/bamboo-pull-requests)"
+  	end
+  end
+
+  erb :'manage-hooks'
+end
+
+post '/manage-hooks' do
+  repo_name = params[:repo_name]
+  hook_events = params[:hook_events]
+  hook_url = params[:hook_url]
+  hook_id = params[:hook_id]
+
+  if repo_name and hook_id
+  	@github_client.test_hook(repo_name, hook_id)
+
+  	redirect '/manage-hooks?repo_name=' + repo_name
+  elsif repo_name and hook_events and hook_url
+	@github_client.create_hook(repo_name, 'web', {
+      :url => hook_url,
+      :content_type => 'json'
+  	}, {
+      :events => hook_events.split(','),
+      :active => true
+    })
+
+	@message = "This event hook is added successfully. You can check it in the list section."
+  end
+
+  erb :'manage-hooks'
+end
+
+delete '/manage-hooks' do
+	repo_name = params[:repo_name]
+	hook_id = params[:hook_id]
+
+	if repo_name and hook_id
+	  @github_client.remove_hook(repo_name, hook_id)
+
+	  redirect '/manage-hooks?repo_name=' + repo_name
+	end
+end
+
