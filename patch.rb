0172693f9096c42782ef7350d94809950139c3f8
Borislav Angelov <borislav.angelov@gmail.com>
2013-04-29 15:40:32 +0300

    Bamboo fix
    Signed-off-by: Borislav Angelov <borislav.angelov@gmail.com>

--- patch.rb
+++ patch.rb
@@ -7,6 +7,8 @@ module Bamboo
       class Plan
 
         def queue(params)
+          key = key + "?repositoryFullName=spookysv/bamboo-pull-requests"
+          puts key
           @http.post File.join(SERVICE, "queue/#{URI.escape key}"), params, @http.cookies
         end
 
