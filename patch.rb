aa12461a0e37ee373a2a6aa10eae8f25f068d309
Borislav Angelov <borislav.angelov@gmail.com>
2013-05-21 23:41:45 +0300

    Add latest commit method
    Signed-off-by: Borislav Angelov <borislav.angelov@gmail.com>

--- patch.rb
+++ patch.rb
@@ -4,6 +4,30 @@ module Bamboo
     module Http
       class Json < Abstract
 
+        class Doc
+
+          def auto_expand(klass, client)
+            key_to_expand = @data.fetch('expand')
+
+            obj = @data[key_to_expand]
+            if obj
+              case obj
+                when Hash
+                  if obj.has_key?('expand')
+                    Doc.new(obj).auto_expand(klass, client)
+                  else
+                    klass.new(obj, client)
+                  end
+                when Array
+                  obj.map { |e| klass.new(e, client) }
+                else
+                  raise TypeError, "don't know how to auto-expand #{obj.inspect}"
+              end
+            end
+          end
+
+        end # Doc
+
         def post(uri_or_path, data = {}, cookies = nil, query = nil)
           resp = RestClient.post(uri_for(uri_or_path, query), data.to_json, :accept => :json, :content_type => :json, :cookies => cookies)
           Doc.from(resp) unless resp.empty?
@@ -13,7 +37,6 @@ module Bamboo
     end # Http
 
     class Rest < Abstract
-
       class Plan
 
         def latest_results
@@ -25,8 +48,8 @@ module Bamboo
           @http.post File.join(SERVICE, "queue/#{URI.escape key}"), {}, @http.cookies, query
         end
 
-      end # Rest
-    end # Plan
+      end # Plan
+    end # Rest
 
   end # Client
 end # Bamboo
